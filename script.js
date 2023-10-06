c93fbd655220e7db489d06b81a24ba2405ee5c18
TheShailendra <98747865+TheShailendra@users.noreply.github.com>
2023-10-06 22:29:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const typed = new Typed('.multiple-text',{
+    strings:['ECE Student of NITH ','Frontend developer','software developer(DSA)'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
